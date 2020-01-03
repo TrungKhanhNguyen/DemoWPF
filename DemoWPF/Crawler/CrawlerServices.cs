2fdcdcef7f577db93fe094fd636b8d5c486ca526
Michael C <khanhnt92@gmail.com>
2020-01-03 15:32:40 +0700

    a

---
+++ DemoWPF/Crawler/CrawlerServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWPF.Crawler
+{
+    public class CrawlerServices
+    {
+        public string getCrawlerParameter(CrawlerObject crawlerObject)
+        {
+            StringBuilder getData = new StringBuilder("/");
+            getData.Append("searchType=NORMAL");
+            getData.Append("&lang=vt");
+            return getData.ToString();
+        }
+    }
+}
